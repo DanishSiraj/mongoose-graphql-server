2f02049874061c9ff2122281d5755fdf4376bcd9
Danish Siraj <danishsiraj38@gmail.com>
2022-10-03 22:57:05 +0530

    Add: populations by model field refs

--- utils/generateSchema.js
+++ utils/generateSchema.js
@@ -25,6 +25,45 @@ const generateSchema = (mongoose) => {
     const model = models[composedTypeName];
     const composedType = composedTypes[composedTypeName];
     const schemaFields = Object.keys(model.schema.paths);
+
+    //Populate using refs
+    Object.keys(model.schema.tree).map((key) => {
+      if (model.schema.tree[key].ref) {
+        const refName = model?.schema?.tree[key]?.ref?.modelName;
+        const resolverOptions = {
+          resolver: () => composedTypes[refName].mongooseResolvers.findOne(),
+          prepareArgs: {
+            filter: (source) => ({
+              _operators: {
+                _id: {in: source[[key]]},
+              },
+            }),
+          },
+          projection: {[[key]]: true},
+        };
+        //generate
+        composedTypes[composedTypeName].addRelation(key, resolverOptions);
+      }
+
+      if (model.schema.tree[key]?.[0]?.ref) {
+        const refName = model.schema.tree[key]?.[0]?.ref?.modelName;
+        const resolverOptions = {
+          resolver: () => composedTypes[refName].mongooseResolvers.findMany(),
+          prepareArgs: {
+            filter: (source) => ({
+              _operators: {
+                _id: {in: source[[key]]},
+              },
+            }),
+          },
+          projection: {[[key]]: true},
+        };
+        //generate
+        composedTypes[composedTypeName].addRelation(key, resolverOptions);
+      }
+    });
+
+    //Populate using virutals
     Object.keys(model.schema.virtuals).map((virtualName) => {
       const virtual = model.schema.virtuals[virtualName];
       if (virtual?.options?.ref) {
@@ -59,6 +98,9 @@ const generateSchema = (mongoose) => {
                 limit: isSingle ? 1 : null,
               }),
             },
+            projection: {
+              [[searchField]]: true,
+            },
           };
         } else {
           resolverOptions = {
@@ -69,6 +111,9 @@ const generateSchema = (mongoose) => {
                   [[searchField]]: {in: source[[searchInField]]},
                 },
               }),
+            },
+            projection: {
+              [[searchField]]: true,
             },
           };
         }
