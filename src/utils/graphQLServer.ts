cd5d779925549639f8374b5a563eca389d22d82b
Danish Siraj <danishsiraj38@gmail.com>
2022-10-08 17:17:52 +0530

    Add: use apollo server as express middleware

--- src/utils/graphQLServer.ts
+++ src/utils/graphQLServer.ts
@@ -1,4 +1,4 @@
-import express, {Application} from 'express';
+import express, {Application, Router} from 'express';
 import {ApolloServer, Config, ExpressContext} from 'apollo-server-express';
 import {express as voyagerMiddleware} from 'graphql-voyager/middleware/index.js';
 import {GraphQLSchema} from 'graphql';
@@ -25,4 +25,30 @@ const createGraphQLServer = async (
   return app;
 };
 
-export default createGraphQLServer;
+const createGraphQLMiddleware = async (
+  config: GraphQLSchema | Config<ExpressContext>
+) => {
+  const router: Router = Router();
+
+  let configuration: any =
+    config instanceof GraphQLSchema
+      ? {
+          schema: config,
+          introspection: true,
+          plugins: [],
+        }
+      : config;
+
+  const server = new ApolloServer(configuration);
+
+  await server.start();
+
+  server.applyMiddleware({
+    app: router as express.Application,
+    path: '/graphql',
+  });
+  router.use('/voyager', voyagerMiddleware({endpointUrl: '/graphql'}));
+  return router;
+};
+
+export {createGraphQLServer, createGraphQLMiddleware};
